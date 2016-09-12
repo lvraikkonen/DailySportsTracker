77e3f3d78008a22c012d2b1a79534b9f1e1365fe
lvraikkonen <claus.lv@hotmail.com>
2016-09-12 17:34:17 +0800

    Complete Scrapy to crawl data in aqicn.org

--- WeatherCondition/AirQuality/AirQuality/spiders/aqispdier.py
+++ WeatherCondition/AirQuality/AirQuality/spiders/aqispdier.py
@@ -1,34 +1,66 @@
 # -*- coding: utf-8 -*-
 
-from scrapy.spiders import CrawlSpider
+from scrapy.contrib.spiders import CrawlSpider
 from AirQuality.items import AirqualityItem
 from scrapy.selector import Selector
-from datetime import datetime
+import datetime
+import time
+from dateutil import parser
 import re
+
+
+ISOTIMEFORMAT='%Y-%m-%d %X'
 
 
 class AirQualitySpider(CrawlSpider):
     name = "AqiSpider"
+    download_delay = 2
     allowed_domains = ['aqicn.org']
     start_urls = ['http://aqicn.org/city/beijing/en/']
 
     def parse(self, response):
+        # filename = response.url.split("/")[-2] + '.html'
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body)
+
         sel = Selector(response)
-        updateTime = sel.xpath(
-            '//div[@style="font-size:16px;font-weight:light;;"][1]/text()').extract()
-        updateTime = updateTime[0].split()[3]
+        secondsDiff = long(float(sel.xpath('//*[@id="aqiwgtutime"]/@val').extract()[0]))
+        updatetime = time.strftime(ISOTIMEFORMAT, time.localtime(secondsDiff))
+        # parse str into datetime
+        updatetime = parser.parse(updatetime)
+        city = sel.xpath('//*[@id="city0"]/span[1]/text()').extract()[0]
+        aqivalue = int(sel.xpath('//*[@id="aqiwgtvalue"]/text()').extract()[0])
+        aqilevel = sel.xpath('//*[@id="aqiwgtinfo"]/text()').extract()[0]
 
-        sites = sel.xpath('//div[@class="aqivalue"][1]')
-        #///*[@id="cur_pm25"]
-        city = sel.xpath('//title')
+        pm25 = int(sel.xpath('//*[@id="cur_pm25"]/text()').extract()[0])
+        pm10 = int(sel.xpath('//*[@id="cur_pm10"]/text()').extract()[0])
+        o3 = int(sel.xpath('//*[@id="cur_o3"]/text()').extract()[0])
+        no2 = int(sel.xpath('//*[@id="cur_no2"]/text()').extract()[0])
+        so2 = int(sel.xpath('//*[@id="cur_so2"]/text()').extract()[0])
+        co = int(sel.xpath('//*[@id="cur_co"]/text()').extract()[0])
 
-        url = response.url
-        url = url[22:len(url) - 4]
+        temp = int(sel.xpath('//*[@id="cur_t"]/span/text()').extract()[0])
+        dew = int(sel.xpath('//*[@id="cur_d"]/span/text()').extract()[0])
+        pressure = int(sel.xpath('//*[@id="cur_p"]/text()').extract()[0])
+        humidity = int(sel.xpath('//*[@id="cur_h"]/text()').extract()[0])
+        wind = int(sel.xpath('//*[@id="cur_w"]/text()').extract()[0])
 
-        dt = datetime.now()
+        item = AirqualityItem()
+        item['date'] = updatetime.strftime("%Y%m%d")
+        item['time'] = updatetime.strftime("%H%M%S")
+        item['city'] = city
+        item['aqivalue'] = aqivalue
+        item['aqilevel'] = aqilevel
+        item['pm2_5'] = pm25
+        item['pm10'] = pm10
+        item['co'] = co
+        item['no2'] = no2
+        item['o3'] = o3
+        item['so2'] = so2
+        item['temp'] = temp
+        item['dew'] = dew
+        item['pressure'] = pressure
+        item['humidity'] = humidity
+        item['wind'] = wind
 
-        items = []
-
-        for site in sites:
-            item = AirqualityItem()
-            item['']
+        return item
